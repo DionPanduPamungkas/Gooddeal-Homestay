8a95e05a330a9a97c1b177b960a0d4929f0276ad
Yonn-2k03 <dionpandu9613@gmail.com>
2022-09-14 19:43:42 +0700

    TAMBAH FILE

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+int main () {
+    int i = 2, 
+    int j = 3;
+    {
+        int i = 4,
+        int j = 5;
+        printf("%d%d" ,i, j);
+    }
+        printf("%d%d" ,i, j);
+        return 0;
+}
